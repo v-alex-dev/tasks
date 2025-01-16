6d800f16d0e20d2b24656b3d4dabb7820f7756ef
v-alex-dev <vensalex1991@gmail.com>
2025-01-16 15:15:20 +0100

    Init project

---
+++ taskmanager/taskmanager/urls.py
@@ -0,0 +1,12 @@
+# taskmanager/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('tasks.urls')),  # Chemin explicite pour l'application 'tasks'
+    path('api/', include('accounts.urls')),  # Chemin explicite pour l'application 'accounts'
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
